5a348427e0175f16c551a521692ef667093144ad
Glitch (hello-express) <none>
2020-11-01 00:35:57 +0000

    first

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-// server.js
-// where your node app starts
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const app = express()
+const cors = require('cors');
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+app.use(cors());
+// Connect to MongoDB database
+mongoose
+	.connect("mongodb+srv://brad123:brad123@cluster0.zf9fl.mongodb.net/udemy?retryWrites=true&w=majority", { useNewUrlParser: true })
+	.then(() => {
+		const app = express()
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+    // listen for requests :)
+    const listener = app.listen(process.env.PORT, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+   });
+});  
+  
+  
