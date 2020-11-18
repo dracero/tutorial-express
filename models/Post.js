16410e46310121ae527efd6256023e1941217d2b
dracero <diego.racero@gmail.com>
2020-11-18 16:08:21 -0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
 const schema = mongoose.Schema({
-	title: String,
-	content: String
+	nombre: String,
+	apellido: String,
+	email: String,
+	dni:Number
 })
 
-module.exports = mongoose.model("datos", schema)//el nombre que tiene que ir en el model es el de la collection
+module.exports = mongoose.model("datos", schema)//el nombre que tiene que ir en el model es el de la collection, no puede ser con mayúsculas
