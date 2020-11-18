acef0bba286d69e2846262d1e0c0e4de7e8485f5
dracero <diego.racero@gmail.com>
2020-11-18 13:00:55 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,9 +4,8 @@ const router = express.Router()
 
 // Get all posts
 router.get("/posts", async (req, res) => {
-	const posts = await Post.find({})
+	const posts = await Post.find()
 	res.send(posts)
-	console.log("hola")
 })
 
 module.exports = router
