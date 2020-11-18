dcb0bf546b25024fb7a24db3ffc7a2fc79872762
dracero <diego.racero@gmail.com>
2020-11-18 18:13:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express()
 const cors = require('cors');
 const routes = require("./routes")
 
-app.use(cors());
 // Connect to MongoDB database
 mongoose
 	.connect("mongodb+srv://brad123:brad123@cluster0.zf9fl.mongodb.net/udemy?retryWrites=true&w=majority", { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
+    app.use(cors())		
     app.use("/api", routes)
 
     //this is the server
