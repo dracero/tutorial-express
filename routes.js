1e916257e389c7c2309363b677187a0a8ee83489
dracero <diego.racero@gmail.com>
2020-11-28 17:47:39 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -32,7 +32,7 @@ router.get("/insert", async (req, res) => {
 	let apellido = req.query.apellido;
 	let email = req.query.email;
 	let dni = req.query.dni;
-	const insert = await Post.save({nombre:nombre,apellido:apellido,email:email,dni:dni})
+	const insert = await Post.find({nombre:nombre)//,apellido:apellido,email:email,dni:dni})
 	//http://express-tutorial-20.herokuapp.com/api/insert?nombre=Diego&apellido=Racero&email=diego.racero@hotmail.com&dni=21738764
 	//hay que armar esa URL
 	res.send(insert)
